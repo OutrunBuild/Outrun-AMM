cd8142e147c959b2efd806ebd9a741ebb2034260
Jason <2045330878@qq.com>
2025-03-16 20:17:55 +0800

    use transient

--- src/libraries/ReentrancyGuard.sol
+++ src/libraries/ReentrancyGuard.sol
@@ -5,15 +5,14 @@ pragma solidity ^0.8.28;
  * @dev Outrun's ReentrancyGuard implementation, support transient variable.
  */
 abstract contract ReentrancyGuard {
-    // evm_version = cancun
-    // bool transient locked;
-    uint256 private locked = 1;
+    bool transient locked;
+
     error ReentrancyGuardReentrantCall();
 
     modifier nonReentrant() {
-        require(locked == 1, ReentrancyGuardReentrantCall());
-        locked = 2;
+        require(!locked, ReentrancyGuardReentrantCall());
+        locked = true;
         _;
-        locked = 1;
+        locked = false;
     }
 }
