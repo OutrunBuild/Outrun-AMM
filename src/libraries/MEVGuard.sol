17943e51c1de11d8a4bf1f1eba68e09e307aa09d
Jason <2045330878@qq.com>
2025-03-23 18:23:10 +0800

    Optimize the MEV Guard logic

--- src/libraries/MEVGuard.sol
+++ src/libraries/MEVGuard.sol
@@ -1,23 +1,19 @@
 //SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.28;
 
-import {Nonces} from "@openzeppelin/contracts/utils/Nonces.sol";
 import {Initializable} from "../libraries/Initializable.sol";
 
 /**
  * @dev MEV Guard - Anti-Front Running and MEV
  */
-abstract contract MEVGuard is Nonces, Initializable {
-    struct ExecutionDetail {
-        bool isExecuted;
-        uint248 nonce;
-    }
-
+abstract contract MEVGuard is Initializable {
     uint256 public antiFrontBlockEdge;
 
     uint256 public antiFrontPercentage;
 
-    mapping(uint256 blockNum => ExecutionDetail) private executionDetails;
+    uint256 private nonce;
+
+    mapping(uint256 blockNum => bool) private executionStates;
 
     function __OutrunMEVGuard_init(uint256 _antiFrontBlockEdge, uint256 _antiFrontPercentage) internal onlyInitializing {
         antiFrontBlockEdge = _antiFrontBlockEdge;
@@ -35,13 +31,12 @@ abstract contract MEVGuard is Nonces, Initializable {
 
         // Only one transaction can succeed per block before antiFrontBlockEdge, or the previous 
         // successful transaction did not enable anti-MEV.
-        if (executionDetails[currentBlockNum].isExecuted) return false;
+        if (executionStates[currentBlockNum]) return false;
 
         // Anti-Front Running
         if (currentBlockNum < antiFrontBlockEdge) {
-            executionDetails[currentBlockNum].nonce++;
-            
-            if (executionDetails[currentBlockNum].isExecuted) return false;
+            uint256 _nonce = nonce + 1;
+            nonce = _nonce;
 
             // Each transaction can purchase a maximum of 1% of the tokens in the liquidity pool
             // before antiFrontBlockEdge.
@@ -50,24 +45,25 @@ abstract contract MEVGuard is Nonces, Initializable {
             // Integrating VRF into the Swap process significantly degrades the user experience. 
             // Pseudorandom numbers are sufficient because the parameters for generating random 
             // numbers are complex enough.
-            uint256 latestExecutionNonce = executionDetails[currentBlockNum - 1].nonce;
             uint256 pseudoRandomNum = uint256(keccak256(abi.encodePacked(
                 tx.origin,
                 block.coinbase,
                 block.basefee,
                 block.prevrandao,
                 blockhash(currentBlockNum - 1),
-                latestExecutionNonce,
-                _useNonce(tx.origin)
+                _nonce
             )));
             
             // Success probability is antiFrontPercentage%
-            if (pseudoRandomNum % 100 <= antiFrontPercentage) executionDetails[currentBlockNum].isExecuted == true;
+            if (pseudoRandomNum % 100 <= antiFrontPercentage) {
+                executionStates[currentBlockNum] = true;
+            } else {
+                return false;
+            }
         } else if (antiMEV) {
             // Prevent subsequent transactions of the same trading pair in the current block.
-            executionDetails[currentBlockNum].isExecuted == true;
+            executionStates[currentBlockNum] = true;
         }
-
         return true;
     }
 }
